5a6dd0cf441875c9c1a4d11ced177cecba97fe79
Zyta Slowianska <zytaslowianska@gmail.com>
2024-03-09 15:24:24 +0100

    feat: ft_putnbr_fd finished

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,15 +6,29 @@
 /*   By: zslowian <zslowian@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/09 12:10:51 by zslowian          #+#    #+#             */
-/*   Updated: 2024/03/09 12:23:22 by zslowian         ###   ########.fr       */
+/*   Updated: 2024/03/09 15:23:01 by zslowian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-//TODO: add ft_atoi()
-
 void	ft_putnbr_fd(int n, int fd)
 {
-	ft_putchar_fd((char)n + 48, fd);
+	if (n == -2147483648)
+		ft_putstr_fd("-2147483648", fd);
+	else if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		ft_putnbr_fd(-n, fd);
+	}
+	else if (n < 10)
+	{
+		n += 48;
+		ft_putchar_fd(n, fd);
+	}
+	else
+	{
+		ft_putnbr_fd(n / 10, fd);
+		ft_putnbr_fd(n % 10, fd);
+	}
 }
--- tests/test_ft_putnbr_fd.c
+++ tests/test_ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: zslowian <zslowian@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/09 12:13:45 by zslowian          #+#    #+#             */
-/*   Updated: 2024/03/09 12:25:36 by zslowian         ###   ########.fr       */
+/*   Updated: 2024/03/09 15:22:15 by zslowian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,22 @@ void	test_ft_putnbr_fd(void)
 
 	fd = 1;
 	printf("\nTesting ft_putendl_fd\n");
-	printf("Printing integer 2:\n");
+	printf("\nPrinting integer 2:\n");
 	ft_putnbr_fd(2, fd);
 	ft_putchar_fd('\0', fd);
-	printf("Printing integer 27:\n");
-	ft_putnbr_fd(27, 1);
+	printf("\nPrinting integer 27:\n");
+	ft_putnbr_fd(27, fd);
 	ft_putchar_fd('\0', fd);
-	printf("Printing integer -89:\n");
-	ft_putnbr_fd(-89, 1);
+	printf("\nPrinting integer -89:\n");
+	ft_putnbr_fd(-89, fd);
 	ft_putchar_fd('\0', fd);
-	printf("Printing integer 0:\n");
-	ft_putnbr_fd(0, 1);
+	printf("\nPrinting integer 0:\n");
+	ft_putnbr_fd(0, fd);
+	ft_putchar_fd('\0', fd);
+	printf("\nPrinting integer -2147483648:\n");
+	ft_putnbr_fd(-2147483648, fd);
+	ft_putchar_fd('\0', fd);
+	printf("\nPrinting integer 2147483647:\n");
+	ft_putnbr_fd(2147483647, fd);
 	ft_putchar_fd('\0', fd);
 }
