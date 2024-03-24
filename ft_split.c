b8906b371aeac9144fbd4fbb0e6dd6b6f10630f1
Zyta Slowianska <zytaslowianska@gmail.com>
2024-03-24 16:24:08 +0100

    chore: norm

--- ft_split.c
+++ ft_split.c
@@ -5,15 +5,16 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: zslowian <zslowian@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/24 14:00:19 by zslowian          #+#    #+#             */
-/*   Updated: 2024/03/24 14:00:19 by zslowian         ###   ########.fr       */
+/*   Created: 2024/03/24 16:17:37 by zslowian          #+#    #+#             */
+/*   Updated: 2024/03/24 16:17:37 by zslowian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	word_count(char const *s, char c);
-static void *ft_free(char **strs, int count);
+static void	ft_assign_vars(size_t *i, int *j, int *s);
+static void	*ft_free(char **strs, int count);
+static int	ft_word_count(char const *s, char c);
 
 char	**ft_split(char const *s, char c)
 {
@@ -22,20 +23,18 @@ char	**ft_split(char const *s, char c)
 	int		j;
 	int		word_start;
 
-	i = 0;
-	j = 0;
-	word_start = -1;
-	result = ft_calloc((word_count(s, c) + 1), sizeof(char *));
+	ft_assign_vars(&i, &j, &word_start);
+	result = ft_calloc((ft_word_count(s, c) + 1), sizeof(char *));
 	if (!result)
 		return (0);
 	while (i <= ft_strlen(s))
 	{
-		if(s[i] != c && word_start < 0)
+		if (s[i] != c && word_start < 0)
 			word_start = i;
-		else if((s[i] == c || i == ft_strlen(s)) && word_start >= 0)
+		else if ((s[i] == c || i == ft_strlen(s)) && word_start >= 0)
 		{
 			ft_strlcpy(result[j], (const char *)s, i - word_start + 1);
-			if(!result[j])
+			if (!result[j])
 				return (ft_free(result, j + 1));
 			word_start = -1;
 			j++;
@@ -45,30 +44,14 @@ char	**ft_split(char const *s, char c)
 	return (result);
 }
 
-static int	word_count(char const *s, char c)
+static void	ft_assign_vars(size_t *i, int *j, int *s)
 {
-	int		count;
-	int		i;
-	int		is_word;
-
-	count = 0;
-	i = 0;
-	is_word = 0;
-	while(s[i] != '\0')
-	{
-		if(s[i] != c && is_word = 0)
-		{
-			is_word = 1;
-			count++;
-		}
-		else if (s[i] == c)
-		is_word = 0;
-		i++;
-	}
-	return (count);
+	*i = 0;
+	*j = 0;
+	*s = -1;
 }
 
-static void *ft_free(char **strs, int count)
+static void	*ft_free(char **strs, int count)
 {
 	while (count > 0)
 	{
@@ -77,4 +60,27 @@ static void *ft_free(char **strs, int count)
 	}
 	free(strs);
 	return (0);
+}
+
+static int	ft_word_count(char const *s, char c)
+{
+	int	count;
+	int	i;
+	int	is_word;
+
+	count = 0;
+	i = 0;
+	is_word = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] != c && is_word == 0)
+		{
+			is_word = 1;
+			count++;
+		}
+		else if (s[i] == c)
+			is_word = 0;
+		i++;
+	}
+	return (count);
 }
